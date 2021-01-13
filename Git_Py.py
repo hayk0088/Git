c7111e8453f58ba9e9c81b38561c3a544162248d
hayk0088 <haykosimonyan17@mail.ru>
2021-01-14 00:21:44 +0400

    Polish notation calculator. Version 1.0

--- Git_Py.py
+++ Git_Py.py
@@ -1,5 +1,13 @@
-print("Hello World:")
+word = input("Enter a string, please: ")
+word_copy = word
+count = 0
 
-print(1 + 2)
 
-print("Hayks es qez sirum em: ")
+def counting(word, word_copy,count):
+    if len(word_copy) < 1:
+        return
+    for i in range(len(word_copy)):
+        if word_copy[i] == word[i]:
+            count += 1
+    counting(word_copy[1:])
+
